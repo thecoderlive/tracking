d867ea39e34efe0bba06a5dd0a56a40d9553e544
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-13 11:04:56 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,76 @@
+export const item = {
+    "id": "e877f5f0-280b-4441-8363-289b78ae9630",
+    "search_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/235.png",
+    "search_for_performers": "Country six away themselves early. Especially agency them whose. Family experience college doctor others focus baby citizen. All street meet.",
+    "cross_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/317.png",
+    "application_list": [
+        {
+            "id": "4fd56ef4-915b-495c-b273-2a2594d9212c",
+            "application_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/317.png",
+            "application_name": "after"
+        },
+        {
+            "id": "c2135ade-6353-435f-bab2-d5be68e72d94",
+            "application_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/175.png",
+            "application_name": "carry"
+        },
+        {
+            "id": "f9da6065-ef61-4489-9e95-021b4a403709",
+            "application_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/68.png",
+            "application_name": "month"
+        },
+        {
+            "id": "9a7b2d4f-ab17-4119-807a-2539112f836c",
+            "application_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/134.png",
+            "application_name": "always"
+        },
+        {
+            "id": "f85a5f69-1252-40b9-b035-ad38363b0a1f",
+            "application_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/194.png",
+            "application_name": "call"
+        }
+    ],
+    "on_tour": "visit",
+    "tour_list": [
+        {
+            "id": "34c7a140-9320-4ba7-94d9-c91c28c1a9ff",
+            "tour_logo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/15.png",
+            "bell_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/24.png",
+            "tour_name": "rich",
+            "event_count": 7925,
+            "heart_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/157.png"
+        },
+        {
+            "id": "95171f20-0dfc-4563-a0c1-b856df9ec19a",
+            "tour_logo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/127.png",
+            "bell_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/288.png",
+            "tour_name": "five",
+            "event_count": 2645,
+            "heart_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/164.png"
+        },
+        {
+            "id": "c04be9d8-4223-443e-9cd4-bfa6d35b7063",
+            "tour_logo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/3.png",
+            "bell_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/197.png",
+            "tour_name": "eye",
+            "event_count": 6431,
+            "heart_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/175.png"
+        },
+        {
+            "id": "fcbf4756-5f9e-479a-86c1-2506451809af",
+            "tour_logo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/182.png",
+            "bell_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/167.png",
+            "tour_name": "despite",
+            "event_count": 9276,
+            "heart_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/26.png"
+        },
+        {
+            "id": "6b71d31a-de40-4737-b67c-f066d2b3c617",
+            "tour_logo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/364.png",
+            "bell_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/100.png",
+            "tour_name": "deal",
+            "event_count": 6043,
+            "heart_icon": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/111.png"
+        }
+    ]
+}
